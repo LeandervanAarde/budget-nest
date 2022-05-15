57239b1c4c3c92ab2d24ebc276e68ad659ef135f
LeandervanAarde <200211@virtualwindow.co.za>
2022-05-15 18:13:47 +0200

    Income sideBar added
    Side bar added to the main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Col } from 'react-bootstrap';
 import Navigation from './subcomponents/Navigation/Navigation';
+import IncomeOverview from './subcomponents/IncomeOverview.js/IncomeOverview';
 const Main = () => {
     const [showPage, setShowPage] = useState(false);
     return (
-        <Col sm={12} className='contentCon'>
-            <Navigation/>
+        <>
+            <Navigation />
 
-        </Col>
+            <Col sm={12} md={2} className="content">
+                <IncomeOverview />
+            </Col>
+
+            <Col sm={12} md={10} className="content">
+                <h2>MONTHLY INCOME</h2>
+                <Col md={12}></Col>
+            </Col>
+
+        </>
     );
 };
 
---
+++ src/components/subcomponents/IncomeOverview.js/IncomeOverview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+const IncomeOverview = () => {
+    return (
+        <Col sm={12} className="IncomeOvervw">
+        
+        </Col>
+    );
+};
+
+export default IncomeOverview;
